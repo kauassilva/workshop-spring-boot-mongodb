b9297366b9b4c721a9686dff4534231790e2c1c2
Kaua Silva <kauassilva1@gmail.com>
2023-10-15 19:38:27 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/kauassilva/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.kauassilva.workshopmongo.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.kauassilva.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+// Classe responsável por tratar possíveis erros nas requisições
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
