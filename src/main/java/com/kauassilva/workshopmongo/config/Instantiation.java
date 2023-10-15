8a5a4f352a5dc29e3e23147bb3a8f0e959b3bc8b
Kaua Silva <kauassilva1@gmail.com>
2023-10-15 18:47:17 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/kauassilva/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.kauassilva.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.kauassilva.workshopmongo.domain.User;
+import com.kauassilva.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll(); // Deleta todos os registros do banco de dados
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
